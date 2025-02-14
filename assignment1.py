0dcca2eb5a70b03ad43629cad319750c819e579a
DianeMack22 <tenormack@gmail.com>
2025-02-13 22:48:57 -0600

    Attempt 2

--- assignment1.py
+++ assignment1.py
@@ -1,51 +1,44 @@
 import pandas as pd
 import numpy as np
 from pygam import LinearGAM, s
-import joblib
-from datetime import datetime
 
-# Load training data
+# Load the training data
 train_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
-df_train = pd.read_csv(train_url)
+df_train = pd.read_csv(train_url, parse_dates=['datetime'])
 
-if 'timestamp' in df_train.columns:
-    df_train['timestamp'] = df_train['timestamp'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-    df_train['hour'] = df_train['timestamp'].dt.hour
-    df_train['dayofweek'] = df_train['timestamp'].dt.dayofweek
-else:
-    df_train['hour'] = 0
-    df_train['dayofweek'] = 0
+def feature_engineering(df):
+    """Extract useful time-based features from datetime."""
+    df['hour'] = df['datetime'].dt.hour
+    df['dayofweek'] = df['datetime'].dt.dayofweek
+    df['month'] = df['datetime'].dt.month
+    return df
 
-df_train['is_weekend'] = df_train['dayofweek'].isin([5, 6]).astype(int)
+# Apply feature engineering
+df_train = feature_engineering(df_train)
 
-X_train = df_train[['hour', 'dayofweek', 'is_weekend']]
+# Define independent (X) and dependent (y) variables
+X_train = df_train[['hour', 'dayofweek', 'month']]
 y_train = df_train['trips']
 
-# Define and train the model
-model = LinearGAM(s(0) + s(1) + s(2))
+# Define and train the GAM model
+model = LinearGAM(s(0) + s(1) + s(2))  # Smooth splines for each feature
 modelFit = model.fit(X_train, y_train)
 
-# Save the model for future use
-joblib.dump(modelFit, 'pygam_taxi_model.pkl')
-
-# Load test data
+# Load the test dataset
 test_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv"
-df_test = pd.read_csv(test_url)
+df_test = pd.read_csv(test_url, parse_dates=['datetime'])
 
-if 'timestamp' in df_test.columns:
-    df_test['timestamp'] = df_test['timestamp'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-    df_test['hour'] = df_test['timestamp'].dt.hour
-    df_test['dayofweek'] = df_test['timestamp'].dt.dayofweek
-else:
-    df_test['hour'] = 0
-    df_test['dayofweek'] = 0
+# Apply feature engineering
+df_test = feature_engineering(df_test)
+X_test = df_test[['hour', 'dayofweek', 'month']]
 
-df_test['is_weekend'] = df_test['dayofweek'].isin([5, 6]).astype(int)
-
-X_test = df_test[['hour', 'dayofweek', 'is_weekend']]
-
-# Generate predictions
+# Make predictions for the test period
 pred = modelFit.predict(X_test)
 
-# Save predictions
-np.savetxt("taxi_predictions.csv", pred, delimiter=",")
+# Store predictions in the test dataset
+df_test['predicted_trips'] = pred
+
+# Save predictions to a CSV file
+df_test[['datetime', 'predicted_trips']].to_csv("nyc_taxi_predictions.csv", index=False)
+
+print("Model training and prediction complete. Predictions saved to nyc_taxi_predictions.csv.")
